6707b45d174df36e7273840a314eb4cde64ba626
mattmc3 <mattmc3@gmail.com>
2021-02-18 10:41:57 -0500

    Add functions and lib

---
+++ lib/environment.zsh
@@ -0,0 +1,21 @@
+# environment.zsh
+
+# XDG
+# https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+# https://wiki.archlinux.org/index.php/XDG_Base_Directory
+# https://wiki.archlinux.org/index.php/XDG_user_directories
+
+# Make sure XDG variables are set
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  export XDG_DESKTOP_DIR="$HOME/Desktop"
+  export XDG_DOCUMENTS_DIR="$HOME/Documents"
+  export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+  export XDG_MUSIC_DIR="$HOME/Music"
+  export XDG_PICTURES_DIR="$HOME/Pictures"
+  export XDG_PUBLICSHARE_DIR="$HOME/Public"
+fi
