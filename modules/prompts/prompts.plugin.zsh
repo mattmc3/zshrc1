7f7d8595523d4488e80c98d2a1111d3d23c0a502
mattmc3 <mattmc3@gmail.com>
2021-02-22 13:35:40 -0500

    Change prompt plugin to handle pure submod

--- modules/prompts/prompts.plugin.zsh
+++ modules/prompts/prompts.plugin.zsh
@@ -1,9 +1,4 @@
 # prompts.zsh
 
-[[ -d "${0:h}/external" ]] || mkdir -p "${0:h}/external"
-
-[[ -d "${0:h}/external/pure" ]] ||
-  command git clone --depth 1 https://github.com/sindresorhus/pure.git "${0:h}/external/pure"
-
 fpath+="${0:h}/external/pure"
 autoload -U promptinit; promptinit
