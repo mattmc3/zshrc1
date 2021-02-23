8d969f4ba38597c601cf17ce8f44cc22702e990e
mattmc3 <mattmc3@gmail.com>
2021-02-23 15:19:42 -0500

    Support prompts better

--- modules/prompts/prompts.plugin.zsh
+++ modules/prompts/prompts.plugin.zsh
@@ -1,4 +1,9 @@
 # prompts.zsh
 
 fpath+="${0:h}/external/pure"
-autoload -U promptinit; promptinit
+
+zstyle -s :zebrafish:module:prompt theme themename || themename=pure
+(( $+functions[promptinit] )) || autoload -Uz promptinit
+promptinit
+prompt "$themename"
+unset themename
